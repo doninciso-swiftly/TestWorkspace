83abaca6973ecfa4a8fa18b974736dad6e264a64
Don Inciso <don.inciso@swiftly-workspace>
2025-10-02 11:52:35 +0800

    Initial commit: Add Xcode workspace and projects

---
+++ Client-iOS/Client-iOS/Client_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Client_iOSApp.swift
+//  Client-iOS
+//
+//  Created by Don Inciso on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Client_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Client-macOS/Client-macOS/Client_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Client_macOSApp.swift
+//  Client-macOS
+//
+//  Created by Don Inciso on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Client_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
